20e3be4a009886f876be3c7eb33309ded453d5ef
ParthKole <parthkole29@gmail.com>
2026-01-01 13:19:46 +0530

    setup of middlewares , apierrors ,apiresponses, asynchandlers

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import express, { urlencoded } from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+const app=express();
+
+
+//middl-wares 
+
+//1.if cross origin request occurs then allow only these devices (means cors_origin)
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}))
+
+//2.to handle json data 
+app.use(express.json({limit:"16kb"}))
+
+//3.to handle different url's data 
+app.use(express.urlencoded({extended:true,limit:"16kb"}))
+
+//4.use saved files in public folder(public folder :used to temporarily save the data) 
+app.use(express.static("public"))
+app.use(cookieParser())
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import express from "express";
+const app=express();
+const PORT=process.env.PORT||5000;
 
 dotenv.config(
     {
@@ -7,4 +10,18 @@ dotenv.config(
     }
 )
 
-connectDB();
+
+connectDB()
+.then(()=>{
+    app.listen(PORT, ()=>{
+        console.log(`server is listening on PORT : ${PORT}`)
+    }) 
+    
+    app.on("error",(error)=>{
+        console.log("ERR : ",error);
+        throw error;
+    })
+})
+.catch((err)=>{
+    console.log("mongoDB connction error!!!",err)
+})
